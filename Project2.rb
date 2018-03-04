3d40f3a702dab1c0cfae5932278bb8b307162fc6
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2018-03-04 10:03:07 -0800

    Q1,Q3,Q4 - work, need to create ordered list for chapters in Q2

--- Project2.rb
+++ Project2.rb
@@ -30,36 +30,40 @@
 ################################################################
 # Question 2
 
-# book = {title, [cahpeter1, chapter2, chapter3]}
+class Book
+  attr_accessor :title
+  attr_accessor :chapters
 
-#
-# class Book
-#   attr_accessor :title
-#   attr_accessor :chapters
-#
-#   def initialize()
-#     @book = {}
-#     @book[:chapters] = []
-#   end
-#
-#   def add_chapter(chapter)
-#     @book[:title] = title
-#     @book[:chapters] << chapter
-#   end
-#
-#   def chapters
-#     chapter_count = @book[:chapters].count
-#     puts "Your book: #{title} has #{chapter_count} chapters"
-#   end
-#
-# end
-#
-#
-# book = Book.new
-# book.title = "My Awesome Book"
-# book.add_chapter("My Awesome Chapter 1")
-# book.add_chapter("My Awesome Chapter 2")
-# book.chapters
+  def initialize()
+    @book = {}
+    @book[:chapters] = []
+  end
+
+  def add_chapter(chapter)
+    @book[:title] = title
+    @book[:chapters] << chapter
+  end
+
+  def chapters
+    chapter_count = @book[:chapters].count
+    puts "Your book: #{title} has #{chapter_count} chapters:"
+    count = 0;
+  for  i in @book[:chapters]
+    count += 1
+    list_chapters += "#{count} - ${i}"
+
+    puts @book[:chapters]
+
+  end
+
+end
+
+
+book = Book.new
+book.title = "My Awesome Book"
+book.add_chapter("My Awesome Chapter 1")
+book.add_chapter("My Awesome Chapter 2")
+book.chapters
 
 # Your book: My Awesome Book has 2 chapters:
 # 1. My Awesome Chapter 1
@@ -67,17 +71,7 @@
 
 ################################################################
 # Question 3
-#puts 'Give me a number and I will tell you if it\'s prime number or not'
-#n = gets.chomp.to_i
-# def is_prime? (n)
-#   if n <= 1
-#     puts
-#   else
-#     puts false
-#   end
-# end
-# is_prime(3)
-#
+
 # def is_prime?(n)
 #   if n <= 1
 #     puts false
@@ -89,17 +83,15 @@
 # end
 # is_prime?(29)
 
-
-
 ################################################################
 # Question 4
-major_cities = {BC: ["Vancouver", "Victoria", "Prince George"], AB: ["Edmonton", "Calgary"]}
-
-major_cities.each do |key, value|
-
-
-    value.count
-
-    puts "#{key} has #{value.count} main cities: #{value[0..-2].join(", ") + " and " + value[-1]}"
-
-end
+# major_cities = {BC: ["Vancouver", "Victoria", "Prince George"], AB: ["Edmonton", "Calgary"]}
+#
+# major_cities.each do |key, value|
+#
+#
+#     value.count
+#
+#     puts "#{key} has #{value.count} main cities: #{value[0..-2].join(", ") + " and " + value[-1]}"
+#
+# end
