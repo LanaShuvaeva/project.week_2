cc704a2c87365d16ab447a59ab44ac592025aea3
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2018-03-03 20:40:17 -0800

    Q2 - added chapters to a book

--- Project2.rb
+++ Project2.rb
@@ -3,14 +3,15 @@
 # module HelperMethods
 #
 #   #string = "some interesting string in, the, of, and, or, from"
-#
 #   def titleize #(string)
-#   little_words = %w(in the of and or from)
-#   string.capitalize.gsub(/(\w+)/) do |word|
-#     little_words.include?(word) ? word : word.capitalize
+#     little_words = %w(in the of and or from)
+#     string.capitalize.gsub(/(\w+)/) do |word|
+#       little_words.include?(word) ? word : word.capitalize
+#     end
 #   end
 # end
-# end
+#
+#
 # #puts titleize("some isfs string in, the, of, and, or, from")
 # class Class1
 #   include HelperMethods
@@ -23,23 +24,47 @@
 # c = Class1.new
 # c.titleize("some isfs string in, the, of, and, or, from")
 #
+#
 # Class2.titleize("some isfs string in, the, of, and, or, from")
 
 ################################################################
 # Question 2
-# class Book
-#
-#
-#   attr_title :
-# end
-#
-#
-# book = Book.new
-# book.title = "My Awesome Book"
-# book.add_chapter("My Awesome Chapter 1")
-# book.add_chapter("My Awesome Chapter 2")
-# book.chapters
 
+# book = {title, [cahpeter1, chapter2, chapter3]}
+{:title=>["My Awesome Chapter 1", "My Awesome Chapter 2"], :title=>"My Awesome Book",
+:chapters0=>["My Awesome Chapter 5", "My Awesome Chapter 5"], :title0=>"My Awesome Book 2"}
+
+class Book
+  attr_accessor :title
+  attr_accessor :chapters
+
+  def initialize()
+    @book = {}
+    @book[:chapters] = []
+  end
+
+  def add_chapter(chapter)
+    @book[:title] = title
+    @book[:chapters] << chapter
+  end
+
+  def chapters
+    chapter_count = @book[:chapters].count
+    puts "Your book: #{title} has #{chapter_count} chapters"
+  end
+
+end
+
+
+book = Book.new
+book.title = "My Awesome Book"
+book.add_chapter("My Awesome Chapter 1")
+book.add_chapter("My Awesome Chapter 2")
+book.chapters
+
+# Your book: My Awesome Book has 2 chapters:
+# 1. My Awesome Chapter 1
+# 2. My Awesome Chapter 2
 
 ################################################################
 # Question 3
